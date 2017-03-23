0d8424b0c7f15428f39c6ec9c602f5f5b20feb88
volca <volca@tom.com>
2017-03-23 14:58:53 +0800

    example for abtemp

--- examples/example_parse_abtemp.php
+++ examples/example_parse_abtemp.php
@@ -1,6 +1,9 @@
 <?php
 
 require __DIR__ . '/../vendor/autoload.php';
+
+const TEMPERATURE_UUID = "B5B182C7EAB14988AA99B5C1517008D9";
+
 $sampleFile = __DIR__ . '/sample_data.txt';
 
 $parser = new AprBrother\PacketParser();
@@ -16,6 +19,17 @@ foreach($data as $v) {
         continue;
     }
     $uuidString = $parser->hexString($beacon->uuid);
-    echo "uuid: $uuidString major: $beacon->major minor: $beacon->minor\n";
+
+    if ($uuidString != TEMPERATURE_UUID) {
+        // not temperature sensor
+        continue;
+    }
+
+    // parse battery level and temperature
+    // see http://wiki.aprbrother.com/wiki/ABTemp_Payload_Format
+    $minor      = sprintf("%04X", $beacon->minor);
+    $battery    = hexdec(substr($minor, 0, 2));
+    $temperature = hexdec(substr($minor, 2, 4));
+    echo "uuid: $uuidString major: $beacon->major minor: $beacon->minor battery: $battery temperature: $temperature\n";
 }
 echo "done\n";
