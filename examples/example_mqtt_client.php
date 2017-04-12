f0918c86126de295852c6a267a6c0129b2ae7238
volca <volca@tom.com>
2017-04-12 15:08:41 +0800

    add mqtt example

---
+++ examples/example_mqtt_client.php
@@ -0,0 +1,30 @@
+<?php
+require __DIR__ . '/../vendor/autoload.php';
+
+use AprBrother\PacketParser;
+
+$mqttclient = new Mosquitto\Client;
+$mqttclient->onConnect(function() use ($mqttclient){
+	echo "connect success \n";
+	$mqttclient->subscribe('beacons',0);
+});
+$mqttclient->onMessage(function($message) use($mqttclient){
+	list($meta, $data) = PacketParser::parse($message->payload);
+
+	echo "===== meta ====\n";
+	print_r($meta);
+	echo "===== data ====\n";
+	foreach($data as $v) {
+		$iBeacon = (int)PacketParser::isIbeacon($v);
+		echo "mac: $v->macAddress rssi: $v->rssi iBeacon: $iBeacon adv:";
+		echo PacketParser::hexString($v->rawData);
+		echo "\n";
+	}
+});
+$mqttclient->onLog(function($level,$msg)use($mqttclient){
+//	echo "\n";
+//	echo $msg;
+//	echo "\n";
+});
+$mqttclient->connect('127.0.0.1',1883,60);
+$mqttclient->loopforever();
