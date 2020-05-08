32b0b639b40bed4c34a5564ff8c4b0ce220289d1
yanc <volcas@gmail.com>
2020-05-08 14:17:19 +0800

    re-format code

--- examples/example_gateway4_mqtt.php
+++ examples/example_gateway4_mqtt.php
@@ -13,8 +13,8 @@ $username   = MQTT_USER;
 $password   = MQTT_PASS;
 $mqttclient = new Mosquitto\Client;
 $mqttclient->onConnect(function() use ($mqttclient){
-	echo "connect success \n";
-	$mqttclient->subscribe(TOPIC, 0);
+    echo "connect success \n";
+    $mqttclient->subscribe(TOPIC, 0);
 });
 
 $mqttclient->onMessage(function($message) use($mqttclient){
@@ -23,14 +23,14 @@ $mqttclient->onMessage(function($message) use($mqttclient){
     $devices = $data['devices'];
     unset($data['devices']);
 
-	echo "===== device data ====\n";
+    echo "===== device data ====\n";
     print_r($data);
-	foreach($devices as $v) {
+    foreach($devices as $v) {
         $adv = $parser->parse($v);
         echo "mac: $adv->macAddress rssi: $adv->rssi advertising data: ";
         echo $parser->hexString($adv->rawData);
         echo "\n";
-	}
+    }
 });
 //$mqttclient->setCredentials($username, $password);
 $mqttclient->connect(HOST, PORT, 60);
